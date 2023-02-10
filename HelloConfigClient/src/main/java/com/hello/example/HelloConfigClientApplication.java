a88721d1201617f944643d233f44b109f9ee0a14
rahulrsavant <50170885+rahulrsavant@users.noreply.github.com>
2023-02-10 11:24:42 +0530

    Actuator Added

--- HelloConfigClient/src/main/java/com/hello/example/HelloConfigClientApplication.java
+++ HelloConfigClient/src/main/java/com/hello/example/HelloConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.hello.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class HelloConfigClientApplication {
 
 	public static void main(String[] args) {
